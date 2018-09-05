82ed6cae188e61920da06c1cb21a6fd5b54d3e3e
Joaquin Lomas <joaquinlomas>
2018-09-05 20:24:54 -0300

    parte b

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,30 +1,26 @@
 from modulo import leer_archivo
 
-guion=leer_archivo('guion2.txt')                    #Lectura desde archivo
-lista=[]                                            #Lista para reemplazo
-palabras=guion.split('\n')                          #Separamos el string separando por nueva linea
-filtrado = [i.split(':')[0] for i in palabras]      #Separar todo los strings por los ":" y guardar el primer string en la lista    
-for j in filtrado:                                  #Guardamos en la lista personajes solamente aquellos de la lista filtrado que empiecen con un guion
-    if j.startswith('-'):
-        lista.append(j)
-filtrado2=list(set(lista))                         #Limpieza de personajes repetidos
-lista_personajes=[s.replace('-', '') for s in filtrado2] #Quita los guiones del principio
+guion = leer_archivo('guion2.txt')		#lectura desde archivo                                            #Lista para reemplazo
+lineas = guion.split('\n')
+lineas_personajes = []
+for i in lineas:                                  #Guardamos en la lista personajes solamente aquellos de la lista filtrado que empiecen con un guion
+    if i.startswith('-'):
+        lineas_personajes.append(i)
+#print lineas_personajes
+lista_personajes = [i.split(':')[0] for i in lineas_personajes]
+#print lista_personajes
+personajes = list(set(lista_personajes))
+#print personajes
+personajes_principales = [i.replace('-','') for i in personajes]
+#print personajes_principales
+dialogos = [i.split(':')[1] for i in lineas_personajes]
 
-# b) Crear un diccionario que asocie cada personaje con sus respectivos diaogos en la forma:
-# {personaje1:[dialogo1,dialogo2,...],personaje2:[dialogo1,dialogo2,...],..}
-
-diccionario = {}
-for n in filtrado2:
-    diccionario.append(n)
-    for k in palabras:
-        if k.startswith(n):
-            diccionario[n] = append(k.split(':'))
-            
-            
-    
-
-#if palabras.startswitch(-NOMBRE_PERSONAJE)
-#   diccionario.append(palabras)
-
-
-# c) contar la cantidad de veces que habla cada personaje contando cada aparicion en filtrado2
+d = {}
+lista = []
+for z in personajes:
+	for i in xrange(len(lineas_personajes)):
+		aux = lineas_personajes[i]
+		if aux.startswith(z):
+			lista.append(dialogos[i]) 
+	d[z] = lista
+#print d
